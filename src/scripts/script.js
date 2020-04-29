d6aa4d95337bd998a035f0ab362b738bbf966634
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-04-29 13:58:50 +0300

    Сверстана секция "о нас"

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -18,4 +18,27 @@ var heroSwiper = new Swiper(".swiper-hero", {
   },
 });
 
+var aboutSwiper = new Swiper(".swiper-about", {
+  // Optional parameters
+  loop: false,
+  spaceBetween: 30,
+  slidesPerView: 4,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination-about",
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
 heroSwiper.init;
+aboutSwiper.init;
