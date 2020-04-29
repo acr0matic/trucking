aa551c5413d0407122ac2b50125c14b6059194db
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-04-29 12:49:48 +0300

    Сверстан слайдер в шапке сайте

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,21 @@
+/*global Swiper */
 
+var heroSwiper = new Swiper(".swiper-hero", {
+  // Optional parameters
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+heroSwiper.init;
