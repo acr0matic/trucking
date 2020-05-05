44944565c6950ad02d0d7bfbe814652b9bbee172
Nikita Mateyuk <work.acr0matic@gmail.com>
2020-05-05 15:22:21 +0300

    Верстка: Добавлена навигация и плавность прокрутки

---
+++ src/scripts/smooth-scroll.js
@@ -0,0 +1,26 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    const blockID = anchor.getAttribute("href");
+
+    SmoothScroll(blockID);
+  });
+}
+
+function SmoothScroll(blockID) {
+  var section = document.getElementById(blockID.substr(1));
+
+  if (section)
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+
+  else
+    window.open(blockID, "_self");
+
+    OpenMenu();
+}
+
